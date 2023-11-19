919f7583dcb2037a2c70f28724d4295b4c51adff
taufiqhida <taufiq200305@gmail.com>
2023-11-19 20:58:33 +0700

    selesai

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,6 +1,6 @@
 const{users}= require("../model")
 const utils = require("../utils")
-// const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer")
 
 module.exports={
     register: async(req, res, next)=>{
@@ -24,80 +24,84 @@ module.exports={
             });
         }
     },
-    // resetPassword: async(req, res)=>{
-    //     try {
-    //         const findUser = await users.findFirst({
-    //             where: {
-    //                 email : req.body.email
-    //             }
-    //         })
+    resetPassword: async(req, res)=>{
+        try {
+            console.log(req.body)
+            const findUser = await users.findFirst({
+                where: {
+                    email : req.body.email
+                }
+            })
 
-    //         if(!findUser){
-    //             return res.render("error")
-    //         }
-    //         const encrypt = await utils.cryptPassword(req.body.email)
-    //         await users.update({
-    //             data : {
-    //                resetPasswordToken: encrypt,
-    //             },
-    //             where : {
-    //                 id: findUser.id 
-    //             }
-    //         })
+            if(!findUser){
+                return res.render("error")
+            }
+            const encrypt = await utils.cryptPassword(req.body.email)
+            await users.update({
+                data : {
+                   resetPasswordToken: encrypt,
+                },
+                where : {
+                    id: findUser.id 
+                }
+            })
 
-    //         const transporter = nodemailer.createTransport({
-    //             service: "gmail",
-    //             auth: {
-    //                 user: process.env.EMAIL_USER,
-    //                 pass: process.env.EMAIL_PASSWORD
-    //             }
-    //         });
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                port: 465,
+                secure: false,
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASSWORD
+                }
+            });
 
-    //         const mailOptions = {
-    //             from : "system@gmail.com",
-    //             to: req.body.email,
-    //             subject: "Reset Password",
-    //             html: `p>Reset Password <a href="localhost:3000/set-password/${encrypt}">Click Here</a></p>`
-    //         }
+            const mailOptions = {
+                from : "system@gmail.com",
+                to: req.body.email,
+                subject: "Reset Password",
+                html: `<p>Reset Password <a href="http://localhost:3000/set-password/${encrypt}">Click Here</a></p>` 
+            }
 
-    //         transporter.sendMail(mailOptions, (err)=>{
-    //             if(err){
-    //                 return res.render("error")
-    //             }
-    //             return res.render('success')
-    //         })
-    //     } catch (error) {
-    //         return res.status(500).json({
-    //             error
-    //         });
-    //     }
-    // },
-    // setPassword: async(req, res)=>{
-    //     try {
-    //         const findUser = await users.findFirst({
-    //             where: {
-    //                 resetPasswordToken: req.body.key
-    //             }
-    //         })
-
-    //         if(!findUser){
-    //             return res.render("error")
-    //         }
-    //         await users.update({
-    //             data : {
-    //                password: await utils.cryptPassword(req.body.password),
-    //                resetPasswordToken: null
-    //             },
-    //             where : {
-    //                 id: findUser.id 
-    //             }
-    //         })
-
-    //         return res.render('success')
-    //     } catch (error) {
-    //         return res.status(500).json({
-    //             error
-    //         });
-    //     }
-    // }
+            transporter.sendMail(mailOptions, (err)=>{
+                if(err){
+                    return res.render("error")
+                }
+                return res.render('success')
+            })
+        } catch (error) {
+            return res.status(500).json({
+                error
+            });
+        }
+    },
+    setPassword: async(req, res)=>{
+        try {
+            const findUser = await users.findFirst({
+                where: {
+                    resetPasswordToken: req.body.key
+                }
+            })
+            
+            if(!findUser){
+                return res.render("error")
+            }
+            await users.update({
+                data : {
+                    password: await utils.cryptPassword(req.body.password),
+                    resetPasswordToken: null
+                },
+                where : {
+                    id: findUser.id 
+                }
+            })
+            
+            return res.render('success')
+        } catch (error) {
+            // console.log(error)
+            return res.status(500).json({
+                error
+            });
+        }
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -10,23 +10,27 @@ router.get("/", (req, res)=>{
 router.get("/reset-password", (req, res)=>{
     return res.render("reset-password")
 })
-router.get("/set-password/:key", async (req, res)=>{
+router.get('/set-password/:key', async (req, res) => {
     try {
+        console.log(req.params.key)
         const findData = await users.findFirst({
             where: {
-                resetPasswordToken: req.params.key 
+                resetPasswordToken: req.params.key
             }
         })
-        if(!findData){
-            return res.render("error")
+        if(!findData) {
+            return res.render('error')
         }
 
-        return res.render("set-password", {user : findData})
+        return res.render('set-password', { user: findData});
     } catch (error) {
-        return res.render("error")
+        console.log(error)
+        return res.render('error')
     }
-})
+    
+});
+
 router.post("/api/v1/register", controller.register);
-// router.post("/api/v1/reset-password", controller.resetPassword);
-// router.post("/api/v1/set-password", controller.setPassword);
+router.post("/api/v1/reset-password", controller.resetPassword);
+router.post("/api/v1/set-password", controller.setPassword);
 module.exports = router
