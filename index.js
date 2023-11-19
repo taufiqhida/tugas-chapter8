fc18ea8302c67f8b1abcfddffa5738040e55b4e4
taufiqhida <taufiq200305@gmail.com>
2023-11-19 20:53:14 +0700

    selesai

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express"),
     cors = require("cors"),
     app = express(),
     PORT = process.env.PORT || 3000,
-    routers = require("./routes")
-    // bodyParser = require("body-parser")
+    routers = require("./routes"),
+    bodyParser = require("body-parser")
 
     require("dotenv").config()
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(cors());
 app.use(express.json({strict : false}))
-// app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.urlencoded({extended:true})); 
 app.use(routers);
 app.listen(PORT, () => console.log(`App is running at PORT ${PORT}`))
